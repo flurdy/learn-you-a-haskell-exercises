c7f5babd981a2f3c02d79f336c39715ae11faaa9
ivar <ivar@flurdy.com>
2013-01-29 00:44:58 +0000

    .

--- 2-starting-out.hs
+++ 2-starting-out.hs
@@ -14,26 +14,27 @@ penultimate l = last (init l)
 
 -- Find the element at index k in list l
 -- For example: "findK 2 [0,0,1,0,0,0]" returns 1
-findK k l = undefined
+-- findK k l = last (take k l)
+findK k l = l !! k
 
 -- Determine if list l is a palindrome
-isPalindrome l = undefined
+isPalindrome l = l == reverse l
 
 {-
  - Duplicate the elements in list xs, for example "duplicate [1,2,3]" would give the list [1,1,2,2,3,3]
- - Hint: The "concat [l]" function flattens a list of lists into a single list. 
+ - Hint: The "concat [l]" function flattens a list of lists into a single list.
  - (You can see the function definition by typing ":t concat" into the interpreter. Perhaps try this with other variables and functions)
  -
  - For example: concat [[1,2,3],[3,4,5]] returns [1,2,3,3,4,5]
  -}
-duplicate xs = undefined
+duplicate xs = concat [[a,a] | a <- xs ]
 
 {-
  - Imitate the functinality of zip
  - The function "min x y" returns the lower of values x and y
  - For example "ziplike [1,2,3] ['a', 'b', 'c', 'd']" returns [(1,'a'), (2, 'b'), (3, 'c')]
  -}
-ziplike xs ys = undefined
+ziplike xs ys = [ (x, y) | (x,y) <- xs ys , length tail xs > 0 && length tail ys > 0 ]
 
 -- Split a list l at element k into a tuple: The first part up to and including k, the second part after k
 -- For example "splitAtIndex 3 [1,1,1,2,2,2]" returns ([1,1,1],[2,2,2])
